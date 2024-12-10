91d96e14e3394f4951479a2477b98f8703aec6a4
Yongxin Xu <xu.yongxin@northeastern.edu>
2024-12-09 17:46:53 -0800

    fix: request with credentials

--- src/Kanbas/Courses/client.ts
+++ src/Kanbas/Courses/client.ts
@@ -31,7 +31,7 @@ export const createModuleForCourse = async (courseId: string, module: any) => {
   return response.data;
 };
 export const findUsersForCourse = async (courseId: string) => {
-  const response = await axios.get(`${COURSES_API}/${courseId}/users`);
+  const response = await axiosWithCredentials.get(`${COURSES_API}/${courseId}/users`);
   return response.data;
  };
  
--- src/Kanbas/Dashboard/client.ts
+++ src/Kanbas/Dashboard/client.ts
@@ -1,17 +1,17 @@
 import axios from "axios";
-
+const axiosWithCredentials = axios.create({ withCredentials: true });
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const ENROLLMENTS_API = `${REMOTE_SERVER}/api/enrollments`;
 
 // Fetch all enrollments for a specific user
 export const fetchEnrollments = async (userId: string) => {
-  const response = await axios.get(`${ENROLLMENTS_API}/${userId}`);
+  const response = await axiosWithCredentials.get(`${ENROLLMENTS_API}/${userId}`);
   return response.data;
 };
 
 // Enroll a user in a course
 export const enrollInCourse = async (userId: string, courseId: string) => {
-  const response = await axios.put(`${ENROLLMENTS_API}/${userId}/${courseId}`);
+  const response = await axiosWithCredentials.put(`${ENROLLMENTS_API}/${userId}/${courseId}`);
   return response.data;
 };
 
